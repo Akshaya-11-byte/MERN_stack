93713f45a2c1f1904f9f9b204bf15ae60712893a
Akshaya-11-byte <akkiakshaya1142@gmail.com>
2026-07-17 15:25:35 +0530

    forms

---
+++ js/moves.js
@@ -0,0 +1,55 @@
+const box=document.getElementById("box");
+let x=0;
+let y=0;
+function moveright(){
+    x+=200;
+    box.style.left=x+"px";
+}
+function moveleft(){
+    x-=200;
+    box.style.left=x+"px";
+}
+function moveUp(){
+    y-=20;
+    box.style.top=y+"px";
+}
+function movedown(){
+    y+=200;
+    box.style.top=y+"px";
+}
+
+document.addEventListener(
+    "keydown",
+    (event) => {
+        if (event.key == "ArrowUp") {
+            y -= 30;
+        }
+        if (event.key == "ArrowDown") {
+            y += 30;
+        }
+        if (event.key == "ArrowLeft") {
+            x -= 30;
+        }
+        if (event.key == "ArrowRight") {
+            x += 30;
+        }
+
+        box.style.left = x + "px";
+        box.style.top = y + "px";
+    }
+);
+box.addEventListener("click", () => {
+    box.style.backgroundColor = "red";
+});
+box.addEventListener("dblclick", () => {
+    box.style.backgroundColor = "green";
+});
+box.addEventListener("mouseover", () => {
+    box.style.backgroundColor = "yellow";
+});
+box.addEventListener("mouseleave", () => {
+    box.style.backgroundColor = "blue";
+});
+document.addEventListener("keyup", (event) => {
+    console.log(event.key);
+});

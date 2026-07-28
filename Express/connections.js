626ab6c42565bfa7235efec275e9d7a88ad3df53
Akshaya-11-byte <akkiakshaya1142@gmail.com>
2026-07-28 12:44:12 +0530

    Hiii

--- Express/connections.js
+++ Express/connections.js
@@ -1,17 +1,31 @@
-//import express from "express";
-const express = require("express");
+ import express from "express";
+ import path from "path";
+ import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// const express=require("express");
+// const path = require("path");
 const app = express();
 const PORT = 7000;
 app.get("/",(req,res)=>{
     //res.send("Welcome to my website")
-    res.json({
-        id:420,
-        name:"akki",
-        email:"akki11@gmail.com",
-        course:"MERN STACK"
-    })
-    console.log(req.header);
+    // res.json({
+    //     id:420,
+    //     name:"akki",
+    //     email:"akki11@gmail.com",
+    //     course:"MERN STACK"
+    // })
+    // console.log(req.header);
+    res.sendFile(path.join(__dirname,"public","index.html"))
 });
+app.get("/about",(req,res)=>{
+      res.sendFile(path.join(__dirname,"public","about.html"))
+});
+app.get("/contact",(req,res)=>{
+      res.sendFile(path.join(__dirname,"public","contact.html"))
+});
+
 app.listen(PORT,()=>{
     console.log("Server is running in port 7000")
 
